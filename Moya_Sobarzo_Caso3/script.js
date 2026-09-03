dcee582e4a2d3b0bb91619effc4bc6c12d55ae4f
Nxcxlxs17 <nico.sobarzo@duocuc.cl>
2026-09-02 23:24:34 -0400

    corregir etiquetas duplicadas

--- Moya_Sobarzo_Caso3/script.js
+++ Moya_Sobarzo_Caso3/script.js
@@ -1,6 +1,20 @@
-document.getElementById("loginForm").addEventListener("submit", function(event){
+document.getElementById("cuerpo-login").addEventListener("submit", function(event){
     event.preventDefault();
 
+    const inputUser = document.getElementById("username").value;
+    const inputPass = document.getElementById("password").value;
 
-    
-})
+    const personaELement = document.querySelector(".persona");
+    const id = personaELement.dataset.id;
+    const correctUser = personaELement.dataset.username;
+    const correctPass = personaELement.dataset.password;
+
+    if(inputUser === correctUser && inputPass === correctPass){
+        console.log("Inicio de Sesion exitosa");
+        console.log("ID del usuario registrado:", id);
+        alert("Bienvenido a GameZone");
+    } else {
+        console.log("Datos ingresados incorrectos.")
+        alert("Usuario o contraseña incorrectos");
+    }
+});
