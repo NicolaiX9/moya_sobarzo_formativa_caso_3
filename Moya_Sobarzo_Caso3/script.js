0d2aa9e4b2837b112135d61bf8485ee6c9eafe83
Nxcxlxs17 <nico.sobarzo@duocuc.cl>
2026-09-02 23:04:04 -0400

    arreglando el boton y las funcionalidades del mismo

--- Moya_Sobarzo_Caso3/script.js
+++ Moya_Sobarzo_Caso3/script.js
@@ -1,50 +1,6 @@
-
-const readline = require('readline');
-
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-
-rl.question('Ingresa un valor: ', (respuesta) => { console.log(`El valor ingresado es: ${respuesta}`); rl.close()}); 
+document.getElementById("loginForm").addEventListener("submit", function(event){
+    event.preventDefault();
 
 
-
-
-const readline = require('readline/promises');
-
-const { stdin: input, stdout: output } = require('process');
-
-
-
-async function calcular() {
-
- const rl = readline.createInterface({ input, output });
-
-
-
-
- const dato1 = await rl.question('Ingresa el primer número: ');
-
- const dato2 = await rl.question('Ingresa el segundo número: ');
-
-
-
-
- const num1 = parseFloat(dato1);
-
- const num2 = parseFloat(dato2);
-
- const suma = num1 + num2;
-
-
-
- console.log(`La suma de ${num1} + ${num2} es: ${suma}`);
-
-
-
-
- rl.close();
-
-}
-
-
-
-calcular();
+    
+})
